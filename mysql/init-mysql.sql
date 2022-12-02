4f531404f97a776925aaa2a9a1fcddb798d0cd56
Park Jin <qkrwls6521@gmail.com>
2022-12-02 14:39:28 +0900

    fix: create-table => init-mysql

--- mysql/init-mysql.sql
+++ mysql/init-mysql.sql
@@ -30,3 +30,18 @@ CREATE TABLE comment
     FOREIGN KEY (user_id) REFERENCES user (id),
     FOREIGN KEY (post_id) REFERENCES post (id)
 );
+
+INSERT
+INTO user (id, name, login_id, password)
+VALUES (1, '테스트1', 'test_id_01', 'test1'),
+       (2, '테스트2', 'test_id_02', 'test2')
+
+INSERT
+INTO post (id, post_text, user_id)
+VALUES (1, '테스트1의 게시글입니다.', 1),
+       (2, '테스트2의 게시글입니다.', 2)
+
+INSERT
+INTO comment (id, user_id, text, post_id)
+VALUES (1, 2, '테스트2가 테스트1의 글에 남긴 댓글입니다.', 1),
+       (2, 1, '테스트1이 테스트2의 글에 남긴 댓글입니다.', 2)
