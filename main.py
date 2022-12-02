4b4861d886a37fe3c97695590b9eac303d252d46
Lee Han Kyul <le10e04@gmail.com>
2022-12-02 21:07:36 +0900

    logging 수정1
    BasicConfig를 일단 사용

--- main.py
+++ main.py
@@ -2,30 +2,34 @@ from flask import Flask, render_template
 import pymysql
 
 import logging
-# logging.basicConfig(filename='example.log',
-#                     filemode= "w",
-#                     encoding='utf-8',
-#                     level=logging.DEBUG,
-#                     format='%(asctime)s %(levelname)s:%(message)s',
-#                     datefmt='%m/%d/%Y %I:%M:%S %p')
+logging.basicConfig(
+    level=logging.CRITICAL,
+    format = '{asctime} {levelname:<8} {message}',
+    style= '{',
+    filename='%slog'%__file__[:-2],
+    filemode = 'w'
+)
+logging.debug('This is a debug msg')
+logging.info('This is a info msg')
+
 # 로거 세팅
-logger = logging.getLogger("postprocessor")
-logger.setLevel(logging.DEBUG)
+# logger = logging.getLogger("postprocessor")
+# logger.setLevel(logging.DEBUG)
 
-# 일반 핸들러, 포매터 세팅
-formatter = logging.Formatter("%(asctime)s %(levelname)s:%(message)s")
-handler = logging.StreamHandler()
-handler.setFormatter(formatter)
+# # 일반 핸들러, 포매터 세팅
+# formatter = logging.Formatter("%(asctime)s %(levelname)s:%(message)s")
+# handler = logging.StreamHandler()
+# handler.setFormatter(formatter)
 
-# 크리티컬 이벤트에 대한 핸들러, 포매터 세팅
-formatter_critical = logging.Formatter("!!!!!%(asctime)s %(levelname)s:%(message)s")
-handler_critical = logging.FileHandler("log_event.log")
-handler_critical.setLevel(logging.CRITICAL)
-handler_critical.setFormatter(formatter_critical)
+# # 크리티컬 이벤트에 대한 핸들러, 포매터 세팅
+# formatter_critical = logging.Formatter("!!!!!%(asctime)s %(levelname)s:%(message)s")
+# handler_critical = logging.FileHandler("log_event.log")
+# handler_critical.setLevel(logging.CRITICAL)
+# handler_critical.setFormatter(formatter_critical)
 
-# 각 핸들러를 로거에 추가
-logger.addHandler(handler)
-logger.addHandler(handler_critical)
+# # 각 핸들러를 로거에 추가
+# logger.addHandler(handler)
+# logger.addHandler(handler_critical)
 
 app = Flask(__name__)
 
@@ -43,4 +47,5 @@ def home():
 
 
 if __name__ == '__main__':
+
     app.run()
