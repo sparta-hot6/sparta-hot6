65a36ad97500085e80072ffd2712b9d22b34675b
Lee Han Kyul <le10e04@gmail.com>
2022-12-02 17:20:08 +0900

    logging 기초 형태와 loggerseting을 이용한
    loggerseting은 아직 미완

--- main.py
+++ main.py
@@ -2,8 +2,30 @@ from flask import Flask, render_template
 import pymysql
 
 import logging
-logging.basicConfig(filename='example.log', encoding='utf-8', level=logging.DEBUG, format='%(asctime)s %(message)s')
+# logging.basicConfig(filename='example.log',
+#                     filemode= "w",
+#                     encoding='utf-8',
+#                     level=logging.DEBUG,
+#                     format='%(asctime)s %(levelname)s:%(message)s',
+#                     datefmt='%m/%d/%Y %I:%M:%S %p')
+# 로거 세팅
+logger = logging.getLogger("postprocessor")
+logger.setLevel(logging.DEBUG)
 
+# 일반 핸들러, 포매터 세팅
+formatter = logging.Formatter("%(asctime)s %(levelname)s:%(message)s")
+handler = logging.StreamHandler()
+handler.setFormatter(formatter)
+
+# 크리티컬 이벤트에 대한 핸들러, 포매터 세팅
+formatter_critical = logging.Formatter("!!!!!%(asctime)s %(levelname)s:%(message)s")
+handler_critical = logging.FileHandler("log_event.log")
+handler_critical.setLevel(logging.CRITICAL)
+handler_critical.setFormatter(formatter_critical)
+
+# 각 핸들러를 로거에 추가
+logger.addHandler(handler)
+logger.addHandler(handler_critical)
 
 app = Flask(__name__)
 
