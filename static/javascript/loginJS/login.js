06158874796ec259e39e86d09559f32b9c8880d6
Neo <qwe0238@naver.com>
2022-12-04 03:24:49 +0900

    로그인 db 정보 예시 입력시 로그인성공, MySQL 연동은 아직입니다.

---
+++ static/javascript/loginJS/login.js
@@ -0,0 +1,15 @@
+// const loginTrue = document.querySelector("#loginTrue").innerText
+
+// console.log(Boolean(loginTrue));
+// const loginBoxTemp = `<a href= {{ url_for("login") }} id="loginBox"><button>로그인</button></a>`
+// const logoutBoxTemp = `<a href= {{ url_for("logout") }} id="logoutBox"><button>로그아웃</button></a>`
+
+// if (Boolean(loginTrue) == true){
+//     $('#loginoutBox').empty()
+//     $('#loginoutBox').append(logoutBoxTemp)
+// }
+// else if(Boolean(loginTrue) == false){
+//     $('#loginoutBox').empty()
+//     $('#loginoutBox').append(loginBoxTemp)
+// }
+
