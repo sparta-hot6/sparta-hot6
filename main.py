5636b39bfb70cd2e181ffed621748a8434e11f54
Park Jin <qkrwls6521@gmail.com>
2022-12-04 16:37:19 +0900

    fix: db pw into sparta(parkjin)

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app.secret_key = "hoon"
 # 패스워드는 각자 다를 것이니 수정해서 사용할 것. (passwd='자기비밀번호')
 # 라우트 내부에서 db = connect_db() 형식으로 이용.
 def connect_db():
-    db = pymysql.connect(host="localhost", port=3306, user='root', passwd='930523', db='hotsix', charset='utf8')
+    db = pymysql.connect(host="localhost", port=3306, user='root', passwd='sparta', db='hotsix', charset='utf8')
     return db
 
 @app.route('/')
