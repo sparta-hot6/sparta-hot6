66e212659214fe1b051525142f2d3f038f5b815c
Neo <qwe0238@naver.com>
2022-12-05 23:33:20 +0900

    로그인 기능을 보안상 취약 이슈로 GET에서 POST로 변경, 더나은 코드로 리펙토링

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ db = {
     # 데이터베이스에 접속할 사용자 아이디
     'user': 'root',
     # 사용자 비밀번호
-    'password': '930523',
+    'password': '930523',  # 개발할때 비밀번호 수정해주세요.
     # 접속할 데이터베이스의 주소 (같은 컴퓨터에 있는 데이터베이스에 접속하기 때문에 localhost)
     'host': 'localhost',
     # 관계형 데이터베이스는 주로 3306 포트를 통해 연결됨
@@ -10,5 +10,3 @@ db = {
     # 실제 사용할 데이터베이스 이름
     'database': 'hotsix'
 }
-
-# MySql_DB = f"host={db['host']}, port={db['port']}, user={db['user']}, password={db['password']}, db={db['database']}, charset='utf8'"
