e75f847d6cdea7824534fa91049e8b684f8af22f
Lee Han Kyul <le10e04@gmail.com>
2022-12-06 14:03:22 +0900

    DB에 post 추가

--- page.py
+++ page.py
@@ -24,6 +24,7 @@ def get_posts(offset=0, per_page=10):
 
 @app.route('/', methods=('GET',))
 def index():
+    #page 현재 페이지 번호, per_page 페이지당 보여줄 게시물 개수
     page, per_page, offset = get_page_args(page_parameter="page", per_page_parameter="per_page")
     total = len(posts)
     pagination_posts = get_posts(offset=offset, per_page=per_page)
@@ -35,7 +36,6 @@ def index():
         per_page=per_page,
         pagination=pagination)
 
-    return jsonify(posts)
 
 
 if __name__ == "__main__":
