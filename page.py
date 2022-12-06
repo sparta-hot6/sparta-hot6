607644d0c01d3096c64aba7e87a79ccfff761638
Lee Han Kyul <le10e04@gmail.com>
2022-12-06 19:05:59 +0900

    page.html 필요없는 부분 삭제

--- page.py
+++ page.py
@@ -7,31 +7,34 @@ app = Flask(__name__)
 
 app.template_folder = "templates"
 
-
 # generating data for pagination - 페이지 매김을 위한 데이터 생성
 # users 부분을 mysql과 연동하면 될 듯
 
 posts = dbfunction.get_posts_all()
 
-def get_posts(offset=1, per_page=2):
-    #offset 까먹음 per_page = 보여주고싶은 페이지
-    return posts[offset:offset + per_page]
+
+def get_posts(offset=1, per_page=5):
+    # offset 까먹음 per_page = 보여주고싶은 페이지
+    return posts[offset: offset+per_page]
 
 
 @app.route('/', methods=('GET',))
 def index():
-    #page 현재 페이지 번호, per_page 페이지당 보여줄 게시물 개수
-    page, per_page, offset = get_page_args(page_parameter="page", per_page_parameter="per_page")
+    # page 현재 페이지 번호, per_page 페이지당 보여줄 게시물 개수 offset은 처음을 설정?]
+    page, per_page, offset = get_page_args(page_parameter="page",
+                                           per_page_parameter="per_page")
     total = len(posts)
     pagination_posts = get_posts(offset=offset, per_page=per_page)
-    pagination = Pagination(page=page, per_pagee=per_page, total=total, css_framework='bootstrap5')
+    pagination = Pagination(page=page,
+                            per_pagee=per_page,
+                            total=total,
+                            css_framework='bootstrap5')
     return render_template(
         'page.html',
         posts=pagination_posts,
         page=page,
         per_page=per_page,
         pagination=pagination)
-
 
 
 if __name__ == "__main__":
